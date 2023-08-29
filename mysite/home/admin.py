7aa843bbf6802e6c2ef712f6b61fab9093bf13dc
Furkan <furkan5590@hotmail.com>
2023-08-29 18:16:36 +0300

    blog design changed

--- mysite/home/admin.py
+++ mysite/home/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from .models import *
 
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title' , 'datetime' )
+    list_display_links = ('id' , 'title')
+    list_filter = ('datetime' , )
+    search_fields = ('title', 'description' )
+
+
+
 # Register your models here.
 
-admin.site.register(Blog)
+admin.site.register(Blog, BlogAdmin)
